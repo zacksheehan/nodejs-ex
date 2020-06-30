e832173192434d38d7a5380152e500c8de96b99e
Sandy Koh <sandykoh99@gmail.com>
2020-06-30 10:06:52 +0100

    requested changes

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.use(require('pino-http')({logger: pino}));
 
 app.get('/', (req, res) => {	
   // Use req.log (a `pino` instance) to log JSON:	
-  req.log.info({message: 'Hello from NodeJS Starter Application!'});		
-  res.send('Hello from NodeJS Starter Application!');	
+  req.log.info({message: 'Hello from Node.js Starter Application!'});		
+  res.send('Hello from Node.js Starter Application!');	
 });	
 
 app.get('*', (req, res) => {
